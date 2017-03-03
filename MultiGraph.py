80fea0778ab31056a5668e22ed3a31a0d689f810
pl-kabaradjian <pierre.louis.kaba@gmail.com>
2017-03-03 14:10:18 +0100

    adding MultiGraph object, LZMA compression + cleaning files oOK

---
+++ MultiGraph.py
@@ -0,0 +1,16 @@
+import networkx as nx
+from GraphTools import *
+
+class MultiGraph:
+    def __init__(self):
+        self.nodes = [{}]
+        self.graphs = [nx.Graph()]
+        self.fieldnames = []
+
+    def load(self, filename="MultiGraph.zip"):
+        # TO-DO
+        print('')
+
+    def save(self):
+        # TO-DO
+        print('')
